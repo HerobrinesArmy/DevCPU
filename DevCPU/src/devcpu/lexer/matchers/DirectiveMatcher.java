7f5d25e684c03bb8c032116bff1a5be88ad04cc7
Herobrine <Herobrine@outlook.com>
2013-03-09 05:53:06 +0100

    Limit directives matched by lexer to a set of "allowed" directives

--- DevCPU/src/devcpu/lexer/matchers/DirectiveMatcher.java
+++ DevCPU/src/devcpu/lexer/matchers/DirectiveMatcher.java
@@ -13,7 +13,13 @@ import devcpu.lexer.tokens.LexerToken;
 
 public class DirectiveMatcher implements LexerTokenMatcher {
 	private static DirectiveMatcher matcher = new DirectiveMatcher();
-	private Pattern pattern = Pattern.compile("\\s*[\\.#]"+ Lexer.REGEX_IDENTIFIER +"\\b[\\s\\,]*");
+	private Pattern pattern = Pattern.compile("\\s*[\\.#]("+ Lexer.REGEX_IDENTIFIER +")\\b[\\s\\,]*", Pattern.CASE_INSENSITIVE);
+	private ArrayList<String> allowedDirectives = new ArrayList<String>();
+	{
+		allowedDirectives.add("include");
+		allowedDirectives.add("import");
+		allowedDirectives.add("define");
+	}
 	
 	@Override
 	public List<LexerTokenMatcher> getFollowTokenMatchers() {
@@ -21,15 +27,20 @@ public class DirectiveMatcher implements LexerTokenMatcher {
 		followTokenMatchers.add(DirectiveParametersMatcher.get());
 		return followTokenMatchers;
 	}
-
+	
 	@Override
 	public MatcherResult match(String text, int offset, int lineOffset) {
 		String s = text.substring(offset);
 		Matcher m = pattern.matcher(s);
 		if (m.find() && m.start() == 0) {
 			String match = m.group();
-			LexerToken token = new DirectiveToken(match, lineOffset + offset, lineOffset + offset + m.end());
-			return new StandardResult(true, new LexerToken[]{token}, offset + m.end(), this);
+			String name = m.group(1);
+			for (String allowed : allowedDirectives) {
+				if (allowed.equalsIgnoreCase(name)) {
+					LexerToken token = new DirectiveToken(match, lineOffset + offset, lineOffset + offset + m.end());
+					return new StandardResult(true, new LexerToken[]{token}, offset + m.end(), this);
+				}
+			}
 		}
 		return new StandardResult(false, null, offset, this);
 	}
